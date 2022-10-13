fb70465c5219f045937faaa3fb9d03d87cffb34a
Ditto Joseph <74483851+dittojoseph@users.noreply.github.com>
2022-10-13 19:11:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Deployed EKS APP through Jenkins CICD v2')
+  res.send('Deployed EKS APP through Jenkins CICD v1')
 });
 
 app.listen(PORT, HOST);
